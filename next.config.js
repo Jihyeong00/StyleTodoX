57f645e0a638f2a0267bff893f8f70c277d1fb00
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-01-16 04:13:59 +0900

    feat: styleX 추가완료

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-}
+const stylexPlugin = require('@stylexjs/nextjs-plugin');
 
-module.exports = nextConfig
+const nextConfig = {
+    // Configure `pageExtensions` to include MDX files
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    // Optionally, add any other Next.js config below
+};
+
+module.exports = stylexPlugin({
+    filename: 'stylex-bundle.css',
+    rootDir: __dirname,
+    useCSSLayers: true,
+})(nextConfig);
